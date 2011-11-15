fe28f19983c79c80596a49472db4d7bd575ad22a
Hugo Sereno Ferreira <bytter@gmail.com>
2011-11-15 01:26:10 +0000

    Having fun with unicode and pimps :P

--- src/core/Main.scala
+++ src/core/Main.scala
@@ -14,6 +14,7 @@ object Main extends App {
   rev.reduceList.reverse foreach println
 
   println (3 ∈ List(1, 2, 3))
+  println ((3 ≠ 2) ∧ ⊥)
 }
 
 sealed abstract class Term {
--- src/pimp/pimp.scala
+++ src/pimp/pimp.scala
@@ -1,9 +1,20 @@
 package pimp
 
-class AnyW(elem: Any) {
-  def ∈[T](xs: Seq[T]): Boolean = { xs contains elem }
+class AnyW(a: Any) {
+  def ∈[T](xs: Seq[T]): Boolean = xs contains a
+  def ≠[T](b: Any): Boolean = a != b
+}
+
+class BoolW(a: Boolean) {
+  def ∨(b: Boolean) = a || b
+  def ∧(b: Boolean) = a && b
 }
 
 object Pimp {
   implicit def anyW[T](xs: Any) = new AnyW(xs)
+  implicit def boolW[T](xs: Boolean) = new BoolW(xs)
+
+  def ¬[T](b: Boolean): Boolean = !b
+  val ⊤ = true
+  val ⊥ = false
 }
